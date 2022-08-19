d735fc3b44950a8f28dc95b4e2c004fd4f900855
nathanm4 <nathan.mukku@gmail.com>
2022-08-19 13:31:44 -0400

    first try at dom methods

---
+++ app.js
@@ -0,0 +1,8 @@
+const firstDomDiv = document.createElement('div');
+firstDomDiv.setAttribute('id', 'firstDomDiv');
+
+const content = document.createElement('div');
+content.classList.add('content');
+
+const firstDomParagraph = document.createElement('P');
+firstDomParagraph.textContent = 'Hey Im red!';
